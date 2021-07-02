444ea995f9c395f345ba4910f24cd27bdedd4098
alxsandroN <anavarrito12@gmail.com>
2021-07-01 19:12:35 -0500

    Se agrego libreria leaflet y se creo componente MapView integrando el MapContainer

---
+++ src/components/MapView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+/*
+--Se tiene que importar la hoja de estilos de leaflet para que se visualice correctamente el mapa
+--Se agrega las dimensiones del mapa llamado .leaflet-container en una hoja de estilos
+ */
+const MapView = () => {
+    return <MapContainer center={{ lat: '19.42885428645738', lng: '-99.16721049943848' }} zoom={10} >
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+    </MapContainer>
+
+}
+export default MapView;
